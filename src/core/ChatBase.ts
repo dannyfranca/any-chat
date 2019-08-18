c3d3ee301754368b28dcc8cb4e5ee3ce8dd97184
danny <danny_vcf@hotmail.com>
2019-08-18 19:09:46 -0300

    Improved load process

--- src/core/ChatBase.ts
+++ src/core/ChatBase.ts
@@ -3,9 +3,10 @@ import { Subject } from 'rxjs'
 import EventCallback from "../types/EventCallback"
 import EventHandler from "./EventHandler"
 import EventMapValue from "../types/EventMapValue"
+import Timeout = NodeJS.Timeout
 
 export default abstract class ChatBase<ChatApi> extends EventHandler {
-  abstract _loader(): Promise<ChatApi>
+  abstract _loader(): Promise<ChatApi> | ChatApi
   
   abstract _eventMap: EventMap
   
@@ -14,20 +15,52 @@ export default abstract class ChatBase<ChatApi> extends EventHandler {
   private _eventsMapped: boolean = false
   private _loaded: boolean = false
   
-  constructor() {
+  protected constructor() {
     super()
     this.init()
   }
   
-  private init(): void {
-    this._loader().then((api: ChatApi) => this._api = api)
+  private async init(): Promise<void> {
+    this.on('load', () => this.ready())
+    this.mapEvents()
+    this._api = await this._loader()
     this.mapEvents()
     this.ready()
   }
   
+  protected ready(): void {
+    this._loaded = true
+  }
+  
+  protected waitFor(test: () => any, then: () => any, testInterval: number = 200, limit: number = 10000): Promise<any> {
+    try {
+      if (test()) return Promise.resolve(then())
+    } catch (e) {
+      return Promise.reject(e)
+    }
+    return new Promise((resolve, reject) => {
+      let interval: Timeout
+      let count = 0
+      try {
+        interval = setInterval(() => {
+          if (count > limit) {
+            clearInterval(interval)
+            reject(`Wait time passed the allowed limit ${limit}`)
+          }
+          if (test()) {
+            clearInterval(interval)
+            resolve(then())
+          }
+          count += testInterval
+        }, testInterval)
+      } catch (e) {
+        reject('Error setting interval')
+      }
+    })
+  }
+  
   protected mapEvents(): void {
-    if (!this._eventsMapped) return
-    if (!(this._api instanceof Object)) throw ChatBase.setError('Chat API not referenced to "this._api" yet')
+    if (this._eventsMapped || !this._api) return
     
     for (const prop of Object.keys(this._eventMap) as Array<keyof EventMap>) {
       const callbackTuple: EventMapValue<any> | undefined = this._eventMap[prop]
@@ -57,10 +90,6 @@ export default abstract class ChatBase<ChatApi> extends EventHandler {
     }
     
     this._eventsMapped = true
-  }
-  
-  protected ready(): void {
-    this._loaded = true
   }
   
   protected attachEvent(event: string, callback: EventCallback<any>): void {
